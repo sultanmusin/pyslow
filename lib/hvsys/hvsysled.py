f1ac25540fe8fdb587e027a0235edaa9e08b8631
Oleg <opetukhov@inr.ru>
2020-12-15 13:21:23 +0300

    fix regiester names

--- lib/hvsys/hvsysled.py
+++ lib/hvsys/hvsysled.py
@@ -18,11 +18,11 @@ class HVsysLED:
     capabilities = {
         "STATUS": 0x00, #               # status -  error i.e. in process of settings - stat=avADC-ADCsetpt, #
         "AUTOREG": 0x02, #              # autoreg - 1 - cell will autoregulate LED output to keep ADC readings at ADCsetpt
-        "FREQ": 0x04, #                 # 2 freq - generator frequency (Hz)
+        "SET_FREQUENCY": 0x04, #                 # 2 freq - generator frequency (Hz)
                                         # remember freq in range (1-1000) -> internal gen; freq out of range (1-1000) (for example -1 from TCL) -> external gen/sync. IF freq=0 -> LED disabled
                                         # should be 0 for normal triggered operation
         "AVERAGE_POINTS": 0x06,         # number of adc measurement points to average in regulation process
-        "DAC_VALUE": 0x08,              # 4 DACval - will be written to LED DAC
+        "SET_AMPLITUDE": 0x08,              # 4 DACval - will be written to LED DAC
         "ADC_SET_POINT": 0x0a,          # 5 PIN ADC setpt - cell will autoregulate LED output to keep ADC readings at ADCsetpt
         "CELL_ID": 0x0c,                # 6 new cell ID
         "AVERAGE_ADC": 0x0e,            # 7 avADC - averaged ADC readings 
@@ -35,7 +35,7 @@ class HVsysLED:
 
     volatile = [
         "STATUS",
-        "DAC_VALUE",
+        "SET_AMPLITUDE",
         "AVERAGE_ADC"
     ]
 
