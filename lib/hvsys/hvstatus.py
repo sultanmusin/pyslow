27a24bdafebfe7b755c07f34e3d6080e1ed6e6c8
Oleg <opetukhov@inr.ru>
2021-02-10 15:17:18 +0300

    Read ramp status

--- lib/hvsys/hvstatus.py
+++ lib/hvsys/hvstatus.py
@@ -23,16 +23,18 @@ from hvsyssupply import HVsysSupply
 
 
 # Module status register: 
-# X3 E10 E9 E8 E7 E6 E5 E4 E3 E2 E1 X3 X3 X3 E ON
+# X3 E10 E9 E8 E7 E6 E5 E4 E3 E2 E1 X3 X3 RAMP E ON
 # X3 = reserved
 # En = Error on channel 3
 # E = Any error
 # ON = HV is on
 
 class HVStatus:
+    N_CHANNELS = 10 
     OFFSET_ON = 0
     OFFSET_ERROR = 1
-    OFFSET_ERROR_CHANNEL = {ch:ch+4 for ch in range(1,11)}  # for 1-based channel number
+    OFFSET_RAMP = 2
+    OFFSET_ERROR_CHANNEL = {ch:ch+4 for ch in range(1,1 + N_CHANNELS)}  # for 1-based channel number
 
     def __init__(self, status_register:int):
         self.status = status_register
@@ -42,6 +44,9 @@ class HVStatus:
     
     def is_error(self): 
         return self.check_bit(HVStatus.OFFSET_ERROR) and self.check_bit(HVStatus.OFFSET_ON) # when it is off the error flag is also always on
+
+    def is_ramp(self):
+        return self.check_bit(HVStatus.OFFSET_RAMP)
 
     def is_error_on_channel(self, ch:int): # 1-based
         if ch not in HVStatus.OFFSET_ERROR_CHANNEL:
@@ -64,3 +69,19 @@ class HVStatus:
             return result
         else:
             return "HV OFF"
+
+    def channel_description(self, ch:int):
+        if ch < 1 or ch > HVStatus.N_CHANNELS:
+            return "unknown channel"
+        elif self.is_error_on_channel(ch):
+            return "ERROR"
+        elif self.is_on() and self.is_ramp():
+            return "Ramp Up"
+        elif self.is_on() and not self.is_ramp():
+            return "HV ON"
+        elif not self.is_on() and self.is_ramp():
+            return "Ramp Down"
+        elif not self.is_on() and not self.is_ramp():
+            return "HV OFF"
+
+            
