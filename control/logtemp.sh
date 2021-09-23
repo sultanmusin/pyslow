67d008fc52a73b2f37b89bdf6c54085e0157248a
Dmitry Borisenko <dmitriy.s.borisenko@ya.ru>
2021-09-23 18:03:42 +0300

    stuff

--- control/logtemp.sh
+++ control/logtemp.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-./poller.py -c PsdSlowControlConfig.xml -a 4 -r 0x1a | grep INFO 2>&1 >> 30.txt
-./poller.py -c PsdSlowControlConfig.xml -a 32 -r 0x1a | grep INFO >> 18.txt
-./poller.py -c PsdSlowControlConfig.xml -a 22 -r 0x1a | grep INFO >> 13.txt
-./poller.py -c PsdSlowControlConfig.xml -a 26 -r 0x1a | grep INFO >> 15.txt
-./poller.py -c PsdSlowControlConfig.xml -a 50 -r 0x1a | grep INFO >> 25.txt
-./poller.py -c PsdSlowControlConfig.xml -a 88 -r 0x1a | grep INFO >> 39.txt
+./poller.py -c PsdSlowControlConfig.xml -b new -a 136 -r 0x28 2>&1 | grep INFO >> 1.txt
+./poller.py -c PsdSlowControlConfig.xml -b new -a 142 -r 0x28 2>&1 | grep INFO >> 4.txt
+./poller.py -c PsdSlowControlConfig.xml -b new -a 150 -r 0x28 2>&1 | grep INFO >> 7.txt
+./poller.py -c PsdSlowControlConfig.xml -b old -a 158 -r 0x28 2>&1 | grep INFO >> 10.txt
+
