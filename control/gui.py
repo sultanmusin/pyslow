ba46e1378cec9e300d0c059546156824e120651d
Oleg <opetukhov@inr.ru>
2021-09-26 23:17:50 +0300

    Displaying correction status in grid

--- control/gui.py
+++ control/gui.py
@@ -651,7 +651,7 @@ class MainWindow(wx.Frame):
                     self.m_gridHV.SetCellValue(int(ch)-1, GRID_COLUMN_REFERENCE, str(hv))    
 
                 self.m_gridHV.SetCellValue(GRID_ROW_PEDESTAL, GRID_COLUMN_REFERENCE, str(active_module_config.hvPedestal))
-                self.m_gridHV.SetCellValue(GRID_ROW_TEMPERATURE, GRID_COLUMN_REFERENCE, str(configuration.reference_temperature))
+                self.m_gridHV.SetCellValue(GRID_ROW_TEMPERATURE, GRID_COLUMN_REFERENCE, "%.2f °C"%(configuration.reference_temperature))
                 self.m_gridHV.SetCellValue(GRID_ROW_SLOPE, GRID_COLUMN_REFERENCE, str(-configuration.temperature_slope))
                 self.m_gridHV.SetCellValue(GRID_ROW_TEMPERATURE, GRID_COLUMN_STATE, "From: %s"%(str(active_module_config.temperature_from_module)))
 
@@ -989,6 +989,9 @@ class MainWindow(wx.Frame):
                 self.m_gridHV.SetCellValue(hv_grid_coords[capability], str_value)
             
             if capability == 'TEMPERATURE':
+                v_correction = part.voltageCorrection()
+                self.m_gridHV.SetCellValue(GRID_ROW_TEMPERATURE, GRID_COLUMN_CORRECTED, "%+.2f V"%(float(v_correction)))
+                self.m_gridHV.SetCellValue(GRID_ROW_TEMPERATURE, GRID_COLUMN_MEAS, "%.2f °C"%(float(str_value)))
                 # calculate and display voltage correction (if needed)
                 correction = part.voltageCorrection()
 
