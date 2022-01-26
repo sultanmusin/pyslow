9a29cc00966ba878836fa050c843ee1951725e79
Oleg <opetukhov@inr.ru>
2022-01-26 15:08:35 +0300

    Row count match

--- control/gui2.py
+++ control/gui2.py
@@ -557,6 +557,11 @@ class MainWindow(QtWidgets.QMainWindow):
             )
             self.groupBoxControl.setTitle("Module %s [%s]" % (moduleConfig.id, partsText))
 
+            for ch in range(moduleConfig.n_channels):
+                self.tableHV.showRow(ch)
+            for ch in range(moduleConfig.n_channels, N_SECTIONS):
+                self.tableHV.hideRow(ch)
+
             self.checkBoxOnline.setChecked(Qt.Checked if moduleConfig.online else Qt.Unchecked)
 
             self.checkBoxOnline.setChecked(Qt.Checked if moduleConfig.online else Qt.Unchecked)
