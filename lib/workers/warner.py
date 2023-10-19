3394b7f317c6caf96a07e8e55c28388267dc1079
sultanmusin <musin.su.as@gmail.com>
2023-10-19 11:26:52 +0300

    Python version fix

--- lib/workers/warner.py
+++ lib/workers/warner.py
@@ -8,7 +8,7 @@ WARNING_SERVERS = ['http://127.0.0.1:9009/warning',
                    'http://192.168.69.1:9009']
 
 
-def send_message(params: dict[str,str]):
+def send_message(params: dict):
     for server in WARNING_SERVERS:
         print(server)
         try:
